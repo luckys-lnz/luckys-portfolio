f97f4c82613d42cafece0087cb01fc048156516b
luckys-lnz <lalucky22@gmail.com>
2025-07-22 12:38:59 +0000

    fix: add modal and defaultOpen props to DropdownMenu in ThemeToggle component

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -8,7 +8,7 @@ export function ThemeToggle() {
   const { setTheme, theme } = useTheme()
 
   return (
-    <DropdownMenu>
+    <DropdownMenu  modal={false} defaultOpen={false}>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="icon" className="relative">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
